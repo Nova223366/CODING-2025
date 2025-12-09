b99e22588e3f7c09a57447bfb0dcfa9290fbb803
Nova223366 <jaiaditya49@gmail.com>
2025-12-09 23:00:28 +0530

    Set_symmetric_difference_HW

---
+++ HW_File/set_symmetric_difference.py
@@ -0,0 +1,12 @@
+# A
+Set1 = {"blue", "green"}
+Set2 = {"blue", "yellow"}
+
+# B
+set1 = {1, 2, 3, 4, 5}
+set2 = {1, 5, 6, 7, 8, 9}
+
+symmetric_difference1 = Set1.symmetric_difference(Set2)
+symmetric_difference2 = set1.symmetric_difference(set2)
+print("\n""In 'A' Symmetric difference between 'Set1 and Set2' is:", symmetric_difference1)
+print("In 'B' Symmetric difference between 'Set1 and Set2' is:", symmetric_difference2,"\n")
