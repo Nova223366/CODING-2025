3df101d04539d71d667902cc30c3a71585f102fd
Nova223366 <jaiaditya49@gmail.com>
2025-12-09 22:42:49 +0530

    Class_work

---
+++ Python_function_and_modules/Arrays_act_03.py
@@ -0,0 +1,8 @@
+import array as arr
+
+array_num = arr.array('i', [1, 3, 5, 3, 7, 9, 2 ])
+print("Original array: "+str(array_num))
+print("Number of occurrences of 3 in the said array: "+ str(array_num.count(3)))
+
+array_num.reverse()
+print("Array after reversing: "+ str(array_num))
