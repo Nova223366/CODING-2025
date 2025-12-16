c01396df1451c36945020818c3f1bff6bfc46c33
Nova223366 <jaiaditya49@gmail.com>
2025-12-16 11:06:12 +0530

    Random password generator

---
+++ HW_File/Random_password_generator.py
@@ -0,0 +1,24 @@
+import random
+import string
+print("Welcome to Random password generator! ")
+print("this can make password above to 62 characters lenths (last limit).")
+print("Make lowercase, uppercase and digits.")
+want = input("Do you want to generate an random password? (yes/no): ").lower()
+if want == 'yes':
+    while want == 'yes':
+        def password_generator(length):
+            lowercase = string.ascii_lowercase
+            uppercase = string.ascii_uppercase
+            digits = string.digits
+            all_characters = lowercase + uppercase + digits 
+            password_list = random.sample(all_characters, length)
+            random.shuffle(password_list)
+            return ''.join(password_list)
+        length = int(input("Enter the length of the password you want: "))
+        password = password_generator(length)
+        print("Generated password is:""\n\n\t", password,"\n")
+        want = input("Do you want to generate another password? (yes/no): ").lower()
+    else:
+        print("If you want you can use it later. Goodbye!")
+else:
+    print("Okay, have a nice day!")
--- JTP/JTP_02.py
+++ JTP/JTP_02.py
@@ -1,9 +1,13 @@
-def name_checker(name):
-    if name in list:
-        return "You were in list"
-    else:
-        return "You were not in list"
-list = ["Rohin", "Aditya", "Aman", "Sammar"]
-name = input("Enter your name: ").capitalize()
-result = name_checker(name)
-print(result, name)
+# def name_checker(name):
+#     if name in list:
+#         return "You were in list"
+#     else:
+#         return "You were not in list"
+# list = ["Rohin", "Aditya", "Aman", "Sammar"]
+# name = input("Enter your name: ").capitalize()
+# result = name_checker(name)
+# print(result, name)
+
+
+country = 'international'
+print(country.split('e'))
