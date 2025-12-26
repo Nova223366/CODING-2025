5d66e2b29f7eb71ba4f38d9a50cb523bfae5b718
Nova223366 <jaiaditya49@gmail.com>
2025-12-26 16:52:36 +0530

    Circle_claculator_HW

---
+++ HW_File/Area_and_perimeter_of_circle_HW.py
@@ -0,0 +1,13 @@
+import math
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+    def area(self):
+        return math.pi * self.radius **2
+    def perimeter(self):
+        return 2*math.pi*self.radius
+    
+r = float(input("Enter the radius of the circle: "))
+c = Circle(r)
+print("Area of the circle: ", c.area())
+print("Perimeter of the circle: ", c.perimeter())
