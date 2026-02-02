c138f2f7b842cd02eaeb21635fa977bcc5d0fd78
Nova223366 <jaiaditya49@gmail.com>
2026-02-02 21:20:19 +0530

    GUI pygame_CW

---
+++ GUI pygame/act1.py
@@ -0,0 +1,9 @@
+from tkinter import *
+# Create the Window
+window = Tk()
+
+# Set the window title and size
+window.title("Demo Window")
+window.geometry('400x300')
+# Start the GUI event loop
+window.mainloop()
